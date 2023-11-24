81757ac054322efcb9f967b9b47f5d25e8ab17e8
Rakitha Nonis <rakitha.nonis@inivosglobal.com>
2023-11-24 10:15:04 +0530

    Revert "added the inivos hackathon project"
    This reverts commit d496aaa10b86799211d982c86bee077c18f86216.

--- Inivos hackathon project/PCAFrontendWeb/src/components/EnvConfig.jsx
+++
@@ -1,8 +0,0 @@
-const settings = require('../config/env.json');
-
-class EnvConfig{
-    constructor() {      
-      this.backendAPIUrl = settings.PCA_BACKEND_API_URL;      
-    }
-}
-export default EnvConfig;
